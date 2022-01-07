c39dde25fb3987cfd365dbaece02e02523620da0
Saloni Gupta <sagupta@gammastack.com>
2022-01-07 14:18:13 +0530

    final

--- checkoccurenceofletters.js
+++ checkoccurenceofletters.js
@@ -1,9 +1,14 @@
+function printans(a) {
+  for (const [key, value] of Object.entries(a)) {
+    process.stdout.write(`${key}${value} `);
+  }
+}
 function count(test) {
   let a = {};
   for (let i = 0; i < test.length; i++) {
     a[test[i]] ? a[test[i]]++ : a[test[i]] = 1;
   }
-  console.log(a);
+  printans(a);
 
 }
 let test = "helloworld";
