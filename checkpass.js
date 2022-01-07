ed89c9bb8e43763b5d88544e45e5afbb80180f17
Saloni Gupta <sagupta@gammastack.com>
2022-01-07 12:38:00 +0530

    changed

--- checkpass.js
+++ checkpass.js
@@ -1,16 +1,13 @@
-function CheckPassword(inputtxt) 
-{ 
-var pattern=  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
-if(inputtxt.match(pattern)) 
-{ 
-// console.log('Correct Password Pattern')
-return true;
+function CheckPassword(inputtxt) {
+    var pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+    if (inputtxt.match(pattern)) {
+        // console.log('Correct Password Pattern')
+        return true;
+    }
+    else {
+        // console.log('Wrong...!')
+        return false;
+    }
 }
-else
-{ 
-// console.log('Wrong...!')
-return false;
-}
-} 
 const string = "Salonigupta@10";
 console.log(CheckPassword(string))
