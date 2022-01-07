892b97751525f02dd5f3ac68cced2c0991000970
Saloni Gupta <sagupta@gammastack.com>
2022-01-07 12:38:00 +0530

    changed

--- checkoccurenceofletters.js
+++ checkoccurenceofletters.js
@@ -1,24 +1,10 @@
-function printans( ans )
-{
-  for( let [ key ,value] of ans)
-  {
-    console.log(`${key}  -  ${value} ` );   
+function count(test) {
+  let a = {};
+  for (let i = 0; i < test.length; i++) {
+    a[test[i]] ? a[test[i]]++ : a[test[i]] = 1;
   }
+  console.log(a);
+
 }
-function count(test)
-{
-    let ans = new Map();
-    for( let i = 0 ;i < test.length;i++)
-    {
-      ans.set(test[i], 0);
-    }
-    for( let i = 0 ;i < test.length ;i++)
-    {
-        let k = ans.get(test[i]);
-        ans.set(test[i], k+1) ;
-    }
-    printans(ans);
- 
-}
-let test =  "helloworld";
-count( test);
+let test = "helloworld";
+count(test);
