9fe6eb347e34583b7e4a329ab9ef379b7c71e800
Saloni Gupta <sagupta@gammastack.com>
2022-01-11 12:30:16 +0530

    Creating Folder

--- assignment-2/checkPassword.js
+++ assignment-2/checkPassword.js
@@ -2,5 +2,5 @@ function CheckPassword(inputtxt) {
     var pattern = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@!#?])([a-zA-Z0-9!@#$%^&*]{8,})$/;
     return inputtxt.match(pattern)
 }
-const string = "Salonigupta@10";
+const string = "Sajalgupta@10";
 console.log(CheckPassword(string))
