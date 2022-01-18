bbeda43a973b7af897ee67a2d52a285ad0df3829
Saloni Gupta <sagupta@gammastack.com>
2022-01-18 17:12:49 +0530

    Error correction

---
+++ assignment-7/CRUDApiExpress/services/services.js
@@ -0,0 +1,64 @@
+const file = require("../data.json")
+const fs = require("fs")
+const path = require('path')
+
+const getDataService = function (data,id, res) {
+    const obj = JSON.parse(data);
+    obj.map((doc, i) => {
+        if (doc.id == id) {
+            return res.json({ message: "data fetched", doc })
+        }
+    })
+    return res.json({ message: "Data Not Found" })
+}
+const getAllDataService = function (data, res) {
+    const obj = JSON.parse(data);
+    return res.json({ message: "Data Fetched", obj })
+
+}
+const postService = function (data, res,newData) {
+    obj = JSON.parse(data);
+    obj.push(newData);
+    json = JSON.stringify(obj);
+    fs.writeFile(path.join(__dirname, '../data.json'), json, 'utf8', (error, result) => {
+        if (error) {
+            res.json({ message: "error" })
+            return;
+        }
+        return res.json({ message: "data created", obj })
+    });
+}
+const putService = function (data, res,newData) {
+    obj = JSON.parse(data);
+    obj.forEach((originalData, i) => {
+        if (newData.id == originalData.id) {
+            originalData.name = newData.name
+        }
+    })
+    json = JSON.stringify(obj);
+    fs.writeFile(path.join(__dirname, '../data.json'), json, 'utf8', (error, result) => {
+        if (error) {
+            res.json({ message: "error" })
+            return;
+        }
+        return res.json({ message: "data updated", obj })
+    });
+}
+const deleteService = function (data, res,id) {
+    obj = JSON.parse(data);
+    const newObj = obj.filter((originalData, i) => {
+        return originalData.id != id
+    })
+    json = JSON.stringify(newObj);
+    fs.writeFile(path.join(__dirname, '../data.json'), json, 'utf8', (error, result) => {
+        if (error) {
+            res.json({ message: "error" })
+            return;
+        }
+        return res.json({ message: "data deleted", newObj })
+    });
+}
+
+module.exports = {
+    getAllDataService, getDataService, postService, putService, deleteService
+}
