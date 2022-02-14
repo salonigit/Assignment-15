4d0e8bc1d125ad60c7fb26f9dfc611271decf664
Saloni Gupta <51018743+salonigit@users.noreply.github.com>
2022-02-14 16:59:54 +0530

    Delete assignment-15 directory

--- assignment-15/CRUDApiExpress/services/getCommentsService.js
+++
@@ -1,9 +0,0 @@
-import db from '../models'
-const Comment = db.comments;
-
-const getCommentsService = async (id) => {
-  const result=await Comment.findByPk(id)
-  return result
-}
-
-export default getCommentsService
--- assignment-15/CRUDApiExpress/services/getTutorialsService.js
+++
@@ -1,9 +0,0 @@
-import db from '../models'
-const Tutorial = db.tutorials;
-
-const getTutorialsService = async (tutorialId) => {
-  const result = await Tutorial.findByPk(tutorialId, { include: ["comments"] })
-  return result
-}
-
-export default getTutorialsService
